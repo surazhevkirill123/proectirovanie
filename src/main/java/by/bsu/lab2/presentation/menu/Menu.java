a7ea0cbdb41aef8a7624ae578eb03b5987a8070c
AntonyKor <antonykor.ak@gmail.com>
2020-03-20 10:39:44 +0300

    Add integration with dbo

--- src/main/java/by/bsu/lab2/presentation/menu/Menu.java
+++ src/main/java/by/bsu/lab2/presentation/menu/Menu.java
@@ -1,10 +1,17 @@
 package by.bsu.lab2.presentation.menu;
 
+import by.bsu.lab2.bisiness.Tour;
+import by.bsu.lab2.bisiness.TourType;
+import by.bsu.lab2.dbo.FileDbProcessor;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 
 public class Menu {
     private Scanner scanner = new Scanner(System.in);
+    private FileDbProcessor processor = new FileDbProcessor();
 
     private int choose(String question, int variantNumber) {
         int action;
@@ -19,88 +26,108 @@ public class Menu {
 
     public void showMainMenu() {
         while (true) {
-            int action = choose("Choose action:\n\t1) Show all\n\t2) Find by ID\n\t3) Create new\n\t4) Exit", 4);
-            switch (action) {
-                case 1:
-                    showAllMenu();
-                    break;
-                case 2:
-                    showFindByIdMenu();
-                    break;
-                case 3:
-                    showEditMenu();
-                    break;
-                default:
-                    return;
+            try {
+                int action = choose("""
+                        Choose action:
+                        \t1) Show all
+                        \t2) Find by ID
+                        \t3) Create new
+                        \t4) Exit""", 4);
+                switch (action) {
+                    case 1:
+                        showAllMenu();
+                        break;
+                    case 2:
+                        showFindByIdMenu();
+                        break;
+                    case 3:
+                        Tour tour = new Tour();
+                        showEditMenu(tour);
+                        processor.create(tour);
+                        break;
+                    default:
+                        return;
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         }
     }
 
-    public void showAllMenu() {
-        // TODO: show all
-    }
+    public void showAllMenu() throws Exception {
+        Tour[] tours = processor.readAll();
 
-    public void showFindByIdMenu() {
-        System.out.print("Enter id: ");
-        int id = scanner.nextInt();
+        if (tours.length == 0) {
+            System.out.println("There's no tours!");
+        }
 
-        // TODO: find by id
-
-        int action = choose("Choose action:\n\t1) Edit\n\t2) Delete\n\t3) Back to main menu" ,3);
-
-        switch (action) {
-            case 1:
-                break;
-            case 2:
-                // TODO: delete
+        for (Tour tour : tours) {
+            System.out.println(tour);
+            System.out.println("\n");
         }
     }
 
-    public void showEditMenu() {
-        boolean isCorrect = true;
+    public void showFindByIdMenu() throws Exception {
+        scanner.nextLine();
+        System.out.print("Enter id: ");
+        String id = scanner.nextLine();
 
-        int type = choose("Choose tour type:\n\t1) Relaxation\n\t2) Excursion\n\t3) Shopping", 3);
-        // TODO: save type
-        System.out.print(type);
+        Tour tour = processor.readById(id);
+        System.out.println(tour);
+
+        int action = choose("""
+                Choose action:
+                \t1) Edit
+                \t2) Delete
+                \t3) Back to main menu""",3);
+
+        switch (action) {
+            case 1:
+                showEditMenu(tour);
+                processor.update(tour);
+                break;
+            case 2:
+                processor.delete(tour);
+        }
+    }
+
+    public void showEditMenu(Tour tour) throws ParseException {
+        int type = choose("""
+                Choose tour type:
+                \t1) Relaxation
+                \t2) Excursion
+                \t3) Shopping""", 3);
+        tour.setTourType(TourType.values()[type - 1]);
 
         int isBurning = choose("Is it burning? 1 = no, 2 = yes", 2);
-        // TODO: save isBurning
-        System.out.print(isBurning);
+        tour.setIsBurning(isBurning == 2);
 
         scanner.nextLine();
 
         System.out.print("Enter name of tour operator: ");
         String tourOperator = scanner.nextLine();
-        // TODO: save tour operator
-        System.out.print(tourOperator);
+        tour.setTourOperator(tourOperator);
 
         System.out.print("Enter tourStartLocation: ");
         String tourStartLocation = scanner.nextLine();
-        // TODO: save tourStartLocation
-        System.out.print(tourStartLocation);
+        tour.setTourStartLocation(tourStartLocation);
 
         System.out.print("Enter tourFinishLocation: ");
         String tourFinishLocation = scanner.nextLine();
-        // TODO: save tourFinishLocation
-        System.out.print(tourFinishLocation);
+        tour.setTourFinishLocation(tourFinishLocation);
 
         System.out.print("Enter cost: ");
         double cost = scanner.nextDouble();
-        // TODO: save cost
-        System.out.print(cost);
+        tour.setCost(cost);
 
         scanner.nextLine();
 
         System.out.print("Enter tourStartTime: ");
         String tourStartTime = scanner.nextLine();
-        // TODO: save tourStartTime
-        System.out.print(tourStartTime);
+        tour.setTourStartTime(new SimpleDateFormat("dd/MM/yyyy").parse(tourStartTime));
 
         System.out.print("Enter tourFinishTime: ");
         String tourFinishTime = scanner.nextLine();
-        // TODO: save tourFinishTime
-        System.out.print(tourFinishTime);
-
-        // TODO: save to db
+        tour.setTourFinishTime(new SimpleDateFormat("dd/MM/yyyy").parse(tourFinishTime));
     }
 }
